a3018706f4675fc7bbf60967542a61899e921fbf
MallarChanda <85064012+MallarChanda@users.noreply.github.com>
2021-11-27 12:31:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+LeftWristX = 0;
+RightWristX = 0;
+
+function setup() {
+video = createCapture(VIDEO);
+video.size(550,500);
+
+canvas = createCanvas(550,550);
+canvas.position(560,150);
+
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotposes);
+}
+
+function modelLoaded() {
+console.log("poseNet is Initialized!");   
+}
+
+function draw() {
+backgound('#2e8b57');
+document.getElementById("square_size").innerHTML = "Width & Height of Square will be = " + difference + "px";
+fill('#F90093');
+stroke('#F90093'); 
+square(noseX,noseY,difference);  
+}
+
+function gotposes(results) {
+if (results.length>0) {
+console.log(results);
+noseX = results[0].pose.nose.x;  
+noseY = results[0].pose.nose.y;
+console.log('nose x= ' + noseX + 'nose y= ' + noseY);
+LeftWristX = results[0].pose.leftWrist.x;
+RightWristX = results[0].pose.rightWrist.x;
+difference = LeftWristX - RightWristX;
+console.log("Left Wrist X= " + LeftWristX + "Right Wrist X= " + RightWristX + "difference= " + difference); 
+}    
+}
